10eba1362c2f1385ee01244fc992b192ee23a20b
ThatGenZGamer48 <78960084+ThatGenZGamer48@users.noreply.github.com>
2021-09-16 14:18:56 +0530

    Created template file for slash command handler

---
+++ command-handler-template.js
@@ -0,0 +1,18 @@
+// The name of the command should be the name of the file
+// The file should be stored in ./commands directory.
+// Thank you :)
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("your-command-name")
+        .setDescription("your-command-description")
+        .addStringOption(option =>
+            option.setName("your-option-name")
+                  .setDescription("your-option-description")
+                  .setRequired(true);
+        ),
+    async execute(interaction) {
+        const option1 = interaction.options.getString("your-option-name")
+        // your code here
+    },
+}; 
